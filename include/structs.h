d2fa45f6675e82606ee5c1ee4fce150289329a8c
Pablo Julian Jimenez Cano <pajimene@made-f0Dr10s15.clusters.42paris.fr>
2024-08-27 14:55:37 +0200

    test

--- include/structs.h
+++ include/structs.h
@@ -6,17 +6,33 @@
 /*   By: pajimene <pajimene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 10:25:20 by pajimene          #+#    #+#             */
-/*   Updated: 2024/08/27 10:36:00 by pajimene         ###   ########.fr       */
+/*   Updated: 2024/08/27 14:54:15 by pajimene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTS_H
 # define STRUCTS_H
 
+enum	e_cmd_type {
+	none,
+	s_quote,
+	d_quote,
+	r_in,
+	r_out,
+	r_heredoc,
+	r_append,
+	o_bracket,
+	c_bracket,
+	pre_exp,
+	and,
+	or,
+	pipe,	
+};
+
 typedef struct s_cmd {
-	char			**content;
+	char			*cmd;
+	enum e_cmd_type	cmd_type;
 	struct s_cmd	*next;
-	struct s_cmd	*prev;
 }		t_cmd;
 
 typedef struct s_struct {
