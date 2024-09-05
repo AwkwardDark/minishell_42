52f69a010c9a65595c78e4fc8dad6b24e11bce18
Pierre Beylouni <pbeyloun@made-f0Ar3s3.clusters.42paris.fr>
2024-09-05 15:50:55 +0200

    minishel.h kinda good

--- include/minishell.h
+++ include/minishell.h
@@ -6,16 +6,77 @@
 /*   By: pbeyloun <pbeyloun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 12:40:22 by pbeyloun          #+#    #+#             */
-/*   Updated: 2024/09/05 15:39:28 by pbeyloun         ###   ########.fr       */
+/*   Updated: 2024/09/05 15:49:14 by pbeyloun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+
+// Pablo 
+# include <unistd.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 # include "structs.h"
 # include "../libft/includes/libft.h"
 
+/*color for the minishell prompt*/
+# define GREEN "\033[0;92m"
+# define RED "\033[0;91m"
+# define GRAS "\033[1m"
+# define RESET "\033[0m"
+
+/*Message error macros, it's useful or it makes the code cleaner?*/
+
+/*Character macros*/
+# define S_QUOTE '\''
+# define D_QUOTE '\"'
+# define SPACE ' '
+# define TAB '\t'//we take in account the tabs for minishell?
+# define O_PAREN '('
+# define C_PAREN ')'
+# define P '|'
+# define I_R '<'
+# define O_R '>'
+# define DOLLAR '$'
+
+extern int	g_signal;
+
+/*Error and Memory managment*/
+void	ft_free_exit(t_data *data);
+
+/*Double Linked List utils*/
+t_token	*ft_lstnew(char *content);
+t_token	*ft_lstlast(t_token *lst);
+void	ft_lstadd_back(t_token **lst, t_token *new);
+void	ft_print_lst(t_token *lst);
+void	ft_free_lst(t_token **lst);
+
+/*Redir*/
+void	ft_redir_check(t_token **lst);
+
+/*Parser, Lexer and Simple Syntax management*/
+int		ft_init_data(char **envp, t_data *data);
+void	ft_error(int code);
+void	ft_parser(char *input, t_data *data);
+void	ft_lexer(char *input, t_data *data);
+void	ft_tokenize(t_token *lst);
+int		ft_quote_syntax(char *input);
+int		ft_operator_syntax(char *input, t_data *data);
+int		ft_parenthesis_syntax(char *input, t_data *data);
+int		ft_grammar_syntax(t_token *lst, t_data *data);
+
+/*Parsing Utils*/
+int		ft_is_quote(char c, t_data *data);
+int		ft_is_special(char *input, int *i, t_data *data);
+int		ft_is_symbol(char c);
+void	ft_token_symbol(char *content, t_token *token);
+
+/*Testing utils*/
+
+// Pierre
 /* src/utils/ls_env.c */
 t_env	*ft_initenv(char *key, char *value);
 void	ft_addlstenv(t_env **env, char *key, char *value);
@@ -24,14 +85,6 @@ void	ft_clrenvnode(t_env *env);
 
 /* src/utils/ft_cpyenv.c */
 t_env	*ft_cpyenv(char **env);
-
-/* src/utils/token.c */
-void	ft_free_lst(t_token **a);
-t_token	*ft_lstnew(char *content, t_type type);
-t_token	*ft_lstlast(t_token *lst);
-void	ft_lstadd_back(t_token **lst, t_token *new);
-void	ft_print_lst(t_token *lst);
-void	display_type(t_type type);
 
 /* src/tree/tree.c */
 t_btree	*init_btree(t_token *token, t_btree *left, t_btree *right);
