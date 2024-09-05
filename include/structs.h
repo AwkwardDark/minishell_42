d3c744b2c8a1a738dffae4538fadbee1e6295789
Pierre Beylouni <pbeyloun@made-f0Ar3s3.clusters.42paris.fr>
2024-09-05 15:46:00 +0200

    resolved kinda struct.h

--- include/structs.h
+++ include/structs.h
@@ -6,7 +6,7 @@
 /*   By: pbeyloun <pbeyloun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 10:25:20 by pajimene          #+#    #+#             */
-/*   Updated: 2024/09/05 15:38:58 by pbeyloun         ###   ########.fr       */
+/*   Updated: 2024/09/05 15:45:23 by pbeyloun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,62 +22,64 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <sys/wait.h>
+# define SIMPLE_COMMAND 2 
+# include <errno.h>
+
 /*Used for tokenize each node in the parsing*/
 typedef enum e_type {
-	//NONE=0?,
-	S_QUOTE=1,
-	d_QUOTE=2,
-	R_IN=3,
-	R_OUT=4,
-	HEREDOC=5,
-	APPEND=6,
-	O_PAR=7,
-	C_PAR=8,
-	PRE_EXP=9,
-	AND=10,
-	OR=11,
-	PIPE=12,
-	WORD=0
-} t_type;
+	WORD=0,
+	IN_R=1,
+	OUT_R=2,
+	HEREDOC=3,
+	APPEND=4,
+	O_PAR=5,
+	C_PAR=6,
+	AND=7,
+	OR=8,
+	PIPE=9,	
+}	t_type;
 
 /*Double linked list that stores all the information
-separated by spaces or special characters/operators*/
+separated by spaces or special characters*/
+//TODO: Add redir, expand or wildcar metadata?
 typedef struct s_token {
 	char			*content;
 	t_type			token_type;
+	//int				expand[2];
+	//int				wildcard;
+	char			*redir;
 	struct s_token	*next;
 	struct s_token	*prev;
-	//int				len;
-	//t_expand 		*expand;
 }		t_token;
 
 /*It stores a copy of the enviroment list, used mainly for
-the export, exand and unset built-ins*/
+the export, expand and unset built-ins*/
 typedef struct s_env {
-	char		*key;
-	char		*value;
-	struct s_env *next;
+	char			*key;
+	char			*value;
+	struct s_env	*next;
 }	t_env;
+
+/*Main structure used for sharing information between the
+parsing and the execution, also used for some small features
+ on error management*/
+typedef struct s_data {
+	struct s_env	*env;
+	struct s_token	*token_lst;
+	char			*input;
+	char			quote_type;
+	char			symbol;
+	int				exit_status;
+	char			*syntax_error;
+}		t_data;
+
+/* Pierre */
 
 typedef struct s_btree {
 	t_token			*token;
 	struct s_btree	*left_child;
 	struct s_btree	*right_child;
 }	t_btree;
-
-/*Main structure used for sharing information between the
-parsing and the execution*/
-typedef struct s_data {
-	struct s_env	*env;
-	struct s_token	*token_lst;
-	char			*input;
-	int				exit_status;
-}		t_data;
-
-/* pipex structure */
-# define PIPE 1
-# define SIMPLE_COMMAND 2 
-# include <errno.h>
 
 typedef struct s_pipe
 {
