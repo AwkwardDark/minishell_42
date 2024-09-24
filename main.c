4f94aaf5ab9afc962b453115b3aaf0380f2f23dc
pablo <pajujica@gmail.com>
2024-09-24 10:49:59 +0200

    nothing

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pbeyloun <pbeyloun@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pajimene <pajimene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 10:30:59 by pajimene          #+#    #+#             */
-/*   Updated: 2024/09/23 15:26:47 by pbeyloun         ###   ########.fr       */
+/*   Updated: 2024/09/24 10:27:22 by pajimene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,28 +63,3 @@ int	main(int argc, char **argv, char **envp)
 	ft_free_exit(data);
 	return (0);
 }
-
-		/* signal(SIGQUIT, SIG_IGN);
-		signal(SIGINT, main_sigint);
-		data->input = readline(GREEN GRAS "minishell ~" RESET);
-		data->exit_status = 0;
-		if (!data->input)
-		{
-			printf("exit\n");
-			break ;
-		}
-		add_history(data->input);
-		if (data->input[0] != '\0' && ft_parser(data->input, data))
-		{
-			tree = create_tokentree(&data->token_lst);
-			data->bin->tree = tree;
-			data->b_tree = tree;
-			exec_btree(tree, data);
-			data->lst_exit_status = data->exit_status;
-			clr_btree(tree);
-			data->token_lst = NULL;
-		}
-		else
-			ft_free_lst(&data->token_lst);
-		free(data->input);
-		data->input = NULL; */
